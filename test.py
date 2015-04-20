93f7d604a87be10565f5e5a4ae42503aeebd3514
Colin <cbott6@gmail.com>
2015-04-19 22:02:10 -0400

    added a sample of what I think the wrapper would be like

---
+++ test.py
@@ -0,0 +1,3 @@
+from wrapper import *
+my_window = create_window()
+my_window.draw_circle(100,100,50)
---
+++ wrapper.py
@@ -0,0 +1,31 @@
+import sys
+if sys.version.startswith('2'):
+    from tkMessageBox import *
+    from Tkinter import *
+    import Tkinter as tk
+else:
+    from tkinter.messagebox import *
+    from tkinter import *
+    import tkinter as tk
+
+def create_window():
+    root = Tk()
+    root.title("Python Canvas")
+    win = Window(root)
+    win.mainloop()
+    return win
+
+class Window(Frame):
+    def __init__(self, master):
+        Frame.__init__(self, master)
+        self.grid()
+        self._setup()
+    def _setup(self):
+        """create a canvas"""
+        self.can = Canvas(self, width=500, height = 500, bg="#ffffff")
+        self.can.grid(row = 0, column = 0)
+    def draw_circle(self,x,y,r):
+        """draw a circle a circle"""
+        self.can.create_oval((x-r,y-r,x+r,y+r))
+
+
