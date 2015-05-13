da97615381e4b9dd07c5c8caabe9712ced4f6729
Colin <cbott6@gmail.com>
2015-05-13 17:44:16 -0400

    gradient

--- advanced_tkinter.py
+++ advanced_tkinter.py
@@ -4,12 +4,31 @@ from wrapper import * #import the library
 
 window = create_window()
 
-for y in range(10,500,20):
-    for x in range(10,500,20):
-        r = random.randint(16,255)
-        g = random.randint(16,255)
-        b = random.randint(16,255)
+def len2(s):
+    s = hex(s)[2:]
+    if len(s) == 1:
+        s = "0"+s
+    return s
 
-        c = "#"+hex(r)[2:]+hex(g)[2:]+hex(b)[2:]
+##for y in range(10,500,10):
+##    for x in range(10,500,10):
+##        r = random.randint(16,255)
+##        g = random.randint(16,255)
+##        b = random.randint(16,255)
+##
+##        c = "#"+hex(r)[2:]+hex(g)[2:]+hex(b)[2:]
+##
+##        window.draw_rectangle(x,y,10,10,c)
+       
+for y in range(0,500,3):
+    for x in range(0,500,3):
+        b=len2(int(y/4)+int((500-x)/4))
+        r=len2(0)
+        g=len2(0)
+        
+        c = "#"+r+g+b
+        
+        window.draw_rectangle(x,y,3,3,c)
 
-        window.draw_rectangle(x,y,10,10,c)
+
+
