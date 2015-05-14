5bee8d26d9995c7d5b435e23548ca4ea41cab835
Colin <cbott6@gmail.com>
2015-05-13 21:32:42 -0400

    little changes

--- advanced_tkinter.py
+++ advanced_tkinter.py
@@ -10,14 +10,14 @@ def len2(s):
         s = "0"+s
     return s
 #Draws a grid of colored squares
-##for y in range(10,500,5):
-##    for x in range(10,500,5):
-##        r=len2(random.randint(0,255))
-##        b=len2(random.randint(0,255))
-##        g=len2(random.randint(0,255))
-##        c = "#"+r+g+b
-##
-##        window.draw_rectangle(x,y,5,5,c,"")
+for y in range(0,500,10):
+    for x in range(0,500,10):
+        r=len2(random.randint(0,255))
+        b=len2(random.randint(0,255))
+        g=len2(random.randint(0,255))
+        c = "#"+r+g+b
+
+        window.draw_rectangle(x,y,8,8,c,"")
 
 #draws a color gradient       
 ##for y in range(0,500,3):
@@ -31,16 +31,16 @@ def len2(s):
 ##        window.draw_rectangle(x,y,3,3,c,"")
 
 #draws concentric circles
-for rad in range(255,0,-2):
-    r=len2(int(255))
-    b=len2(int(rad))
-    g=len2(int(rad))
-    c = "#"+r+g+b
-    
-##    r = len2(random.randint(0,255))
-##    g = len2(random.randint(0,255))
-##    b = len2(random.randint(0,255))
+##for rad in range(255,0,-2):
+##    r=len2(int(255))
+##    b=len2(int(rad))
+##    g=len2(int(rad))
 ##    c = "#"+r+g+b
-    
-    window.draw_circle(250,250,rad,c,"")
+##    
+####    r = len2(random.randint(0,255))
+####    g = len2(random.randint(0,255))
+####    b = len2(random.randint(0,255))
+####    c = "#"+r+g+b
+##    
+##    window.draw_circle(250,250,rad,c,"")
 
